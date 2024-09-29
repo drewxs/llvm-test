2cb357cb1e3e09db07f69269b3488c9bd58491f4
Andrew X. Shah <drewshah0@gmail.com>
2024-09-28 23:55:05 -0600

    init driver ctx/mod

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,21 @@
+#include "constants.h"
 #include "parser.h"
-#include <cctype>
-#include <cstdio>
-#include <cstdlib>
+
+// static void init_mod() {
+//   CONTEXT = std::make_unique<llvm::LLVMContext>();
+//   MODULE = std::make_unique<llvm::Module>("JIT", *CONTEXT);
+//   BUILDER = std::make_unique<llvm::IRBuilder<>>(*CONTEXT);
+// }
 
 int main() {
   fprintf(stderr, "kld> ");
   get_next_token();
 
+  // init_mod();
+
   main_loop();
+
+  MODULE->print(llvm::errs(), nullptr);
 
   return 0;
 }
